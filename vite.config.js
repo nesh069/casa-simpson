3b4048185886be6695bf69b2cde25aa900084108
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-05-15 15:45:49 +0300

    fix : tests fix and deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/casa-simpson/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+  },
 })
